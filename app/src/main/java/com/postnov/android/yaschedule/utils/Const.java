a95ea9ca4d01ce7dcb588442fd659b5a4eef116e
valpostnov <valpostnov@gmail.com>
2016-05-21 05:20:33 +0300

    Update Const.java

--- app/src/main/java/com/postnov/android/yaschedule/utils/Const.java
+++ app/src/main/java/com/postnov/android/yaschedule/utils/Const.java
@@ -5,5 +5,5 @@ package com.postnov.android.yaschedule.utils;
  */
 public class Const
 {
-    public static final String API_KEY = "7242ce30-c8da-4a49-af04-f6853753989d";
+    public static final String API_KEY = "000000000000";
 }
